a24f95b8de50156b44daac176e051757dcc75d33
b1n-ch1kn <albradford2468@gmail.com>
2023-03-24 10:40:12 +1000

    install pre commit

--- fs_nav2/launch/nav.launch.py
+++ fs_nav2/launch/nav.launch.py
@@ -1,47 +1,70 @@
-import launch
-from launch.substitutions import Command, LaunchConfiguration
-import launch_ros
 import os
 
+import launch
+import launch_ros
+from launch.substitutions import Command, LaunchConfiguration
+
+
 def generate_launch_description():
-    pkg_share = launch_ros.substitutions.FindPackageShare(package='fs_nav2').find('fs_nav2')
-    slam_pkg_share = launch_ros.substitutions.FindPackageShare(package='slam_toolbox').find('slam_toolbox')
-    nav2_pkg_share = launch_ros.substitutions.FindPackageShare(package='nav2_bringup').find('nav2_bringup')
+    pkg_share = launch_ros.substitutions.FindPackageShare(package="fs_nav2").find(
+        "fs_nav2"
+    )
+    slam_pkg_share = launch_ros.substitutions.FindPackageShare(
+        package="slam_toolbox"
+    ).find("slam_toolbox")
+    nav2_pkg_share = launch_ros.substitutions.FindPackageShare(
+        package="nav2_bringup"
+    ).find("nav2_bringup")
 
     robot_localization_node = launch_ros.actions.Node(
-        package='robot_localization',
-        executable='ekf_node',
-        name='ekf_filter_node',
-        output='screen',
-        parameters=[os.path.join(pkg_share, 'config/localisation_params.yaml'), {'use_sim_time': LaunchConfiguration('use_sim_time')}]
+        package="robot_localization",
+        executable="ekf_node",
+        name="ekf_filter_node",
+        output="screen",
+        parameters=[
+            os.path.join(pkg_share, "config/localisation_params.yaml"),
+            {"use_sim_time": LaunchConfiguration("use_sim_time")},
+        ],
     )
 
     odom_to_tf_node = launch_ros.actions.Node(
-        package='odom_to_tf_ros2',
-        executable='odom_to_tf',
-        name='odom_to_tf',
-        output='screen',
-        parameters=[os.path.join(pkg_share, 'config/odom2tf_params.yaml'), {'use_sim_time': LaunchConfiguration('use_sim_time')}]
+        package="odom_to_tf_ros2",
+        executable="odom_to_tf",
+        name="odom_to_tf",
+        output="screen",
+        parameters=[
+            os.path.join(pkg_share, "config/odom2tf_params.yaml"),
+            {"use_sim_time": LaunchConfiguration("use_sim_time")},
+        ],
     )
 
     slam_async_launch = launch.actions.IncludeLaunchDescription(
         launch.launch_description_sources.PythonLaunchDescriptionSource(
-            os.path.join(slam_pkg_share, 'launch', 'online_async_launch.py')
+            os.path.join(slam_pkg_share, "launch", "online_async_launch.py")
         ),
-        launch_arguments={'slam_params_file': os.path.join(pkg_share, 'config/slam_params.yaml')}.items(),
+        launch_arguments={
+            "slam_params_file": os.path.join(pkg_share, "config/slam_params.yaml")
+        }.items(),
     )
 
     nav2_launch = launch.actions.IncludeLaunchDescription(
         launch.launch_description_sources.PythonLaunchDescriptionSource(
-            os.path.join(nav2_pkg_share, 'launch', 'navigation_launch.py'),
+            os.path.join(nav2_pkg_share, "launch", "navigation_launch.py"),
         ),
-        launch_arguments={'params_file': os.path.join(pkg_share, 'config/nav2_params.yaml')}.items(),
+        launch_arguments={
+            "params_file": os.path.join(pkg_share, "config/nav2_params.yaml")
+        }.items(),
     )
-    return launch.LaunchDescription([
-        launch.actions.DeclareLaunchArgument(name='use_sim_time', default_value='True',
-                                                    description='Flag to enable use_sim_time'),
-        # odom_to_tf_node,
-        robot_localization_node,
-        slam_async_launch,
-        # nav2_launch
-    ])
+    return launch.LaunchDescription(
+        [
+            launch.actions.DeclareLaunchArgument(
+                name="use_sim_time",
+                default_value="True",
+                description="Flag to enable use_sim_time",
+            ),
+            # odom_to_tf_node,
+            robot_localization_node,
+            slam_async_launch,
+            # nav2_launch
+        ]
+    )
--- nav2_testing/sam_bot_description/launch/display.launch.py
+++ nav2_testing/sam_bot_description/launch/display.launch.py
@@ -1,73 +1,115 @@
-import launch
-from launch.substitutions import Command, LaunchConfiguration
-import launch_ros
 import os
 
+import launch
+import launch_ros
+from launch.substitutions import Command, LaunchConfiguration
+
+
 def generate_launch_description():
-    pkg_share = launch_ros.substitutions.FindPackageShare(package='sam_bot_description').find('sam_bot_description')
-    default_model_path = os.path.join(pkg_share, 'src/description/sam_bot_description.urdf')
-    default_rviz_config_path = os.path.join(pkg_share, 'rviz/urdf_config.rviz')
-    default_nav_params_path = os.path.join(pkg_share, 'config/nav2_params.yaml')
+    pkg_share = launch_ros.substitutions.FindPackageShare(
+        package="sam_bot_description"
+    ).find("sam_bot_description")
+    default_model_path = os.path.join(
+        pkg_share, "src/description/sam_bot_description.urdf"
+    )
+    default_rviz_config_path = os.path.join(pkg_share, "rviz/urdf_config.rviz")
+    default_nav_params_path = os.path.join(pkg_share, "config/nav2_params.yaml")
 
     robot_state_publisher_node = launch_ros.actions.Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}, {'use_sim_time': LaunchConfiguration('use_sim_time')}]
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[
+            {"robot_description": Command(["xacro ", LaunchConfiguration("model")])},
+            {"use_sim_time": LaunchConfiguration("use_sim_time")},
+        ],
     )
     joint_state_publisher_node = launch_ros.actions.Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        name='joint_state_publisher',
-        parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')}]
+        package="joint_state_publisher",
+        executable="joint_state_publisher",
+        name="joint_state_publisher",
+        parameters=[{"use_sim_time": LaunchConfiguration("use_sim_time")}],
     )
     rviz_node = launch_ros.actions.Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        output='screen',
-        arguments=['-d', LaunchConfiguration('rvizconfig'), {'use_sim_time': LaunchConfiguration('use_sim_time')}],
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="screen",
+        arguments=[
+            "-d",
+            LaunchConfiguration("rvizconfig"),
+            {"use_sim_time": LaunchConfiguration("use_sim_time")},
+        ],
     )
     spawn_entity = launch_ros.actions.Node(
-        package='gazebo_ros',
-        executable='spawn_entity.py',
-        arguments=['-entity', 'sam_bot', '-topic', 'robot_description'],
-        output='screen',
+        package="gazebo_ros",
+        executable="spawn_entity.py",
+        arguments=["-entity", "sam_bot", "-topic", "robot_description"],
+        output="screen",
     )
     robot_localization_node = launch_ros.actions.Node(
-        package='robot_localization',
-        executable='ekf_node',
-        name='ekf_filter_node',
-        output='screen',
-        parameters=[os.path.join(pkg_share, 'config/ekf.yaml'), {'use_sim_time': LaunchConfiguration('use_sim_time')}]
+        package="robot_localization",
+        executable="ekf_node",
+        name="ekf_filter_node",
+        output="screen",
+        parameters=[
+            os.path.join(pkg_share, "config/ekf.yaml"),
+            {"use_sim_time": LaunchConfiguration("use_sim_time")},
+        ],
     )
-    world_path=os.path.join(pkg_share, 'world/my_world.sdf'),
+    world_path = (os.path.join(pkg_share, "world/my_world.sdf"),)
 
-    slam_pkg_share = launch_ros.substitutions.FindPackageShare(package='slam_toolbox').find('slam_toolbox')
+    slam_pkg_share = launch_ros.substitutions.FindPackageShare(
+        package="slam_toolbox"
+    ).find("slam_toolbox")
     slam_async_launch = launch.actions.IncludeLaunchDescription(
         launch.launch_description_sources.PythonLaunchDescriptionSource(
-            os.path.join(slam_pkg_share, 'launch', 'online_async_launch.py')
+            os.path.join(slam_pkg_share, "launch", "online_async_launch.py")
         )
     )
-    nav2_pkg_share = launch_ros.substitutions.FindPackageShare(package='nav2_bringup').find('nav2_bringup')
+    nav2_pkg_share = launch_ros.substitutions.FindPackageShare(
+        package="nav2_bringup"
+    ).find("nav2_bringup")
     nav2_launch = launch.actions.IncludeLaunchDescription(
         launch.launch_description_sources.PythonLaunchDescriptionSource(
-            os.path.join(nav2_pkg_share, 'launch', 'navigation_launch.py'),
+            os.path.join(nav2_pkg_share, "launch", "navigation_launch.py"),
         ),
-        launch_arguments={'params_file': default_nav_params_path}.items(),
+        launch_arguments={"params_file": default_nav_params_path}.items(),
     )
-    return launch.LaunchDescription([
-        launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
-                                            description='Absolute path to robot urdf file'),
-        launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
-                                            description='Absolute path to rviz config file'),
-        launch.actions.DeclareLaunchArgument(name='use_sim_time', default_value='True',
-                                                    description='Flag to enable use_sim_time'),
-        launch.actions.ExecuteProcess(cmd=['gzserver', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so', world_path], output='screen'),
-        joint_state_publisher_node,
-        robot_state_publisher_node,
-        spawn_entity,
-        robot_localization_node,
-        rviz_node,
-        # slam_async_launch,
-        # nav2_launch
-    ])
+    return launch.LaunchDescription(
+        [
+            launch.actions.DeclareLaunchArgument(
+                name="model",
+                default_value=default_model_path,
+                description="Absolute path to robot urdf file",
+            ),
+            launch.actions.DeclareLaunchArgument(
+                name="rvizconfig",
+                default_value=default_rviz_config_path,
+                description="Absolute path to rviz config file",
+            ),
+            launch.actions.DeclareLaunchArgument(
+                name="use_sim_time",
+                default_value="True",
+                description="Flag to enable use_sim_time",
+            ),
+            launch.actions.ExecuteProcess(
+                cmd=[
+                    "gzserver",
+                    "--verbose",
+                    "-s",
+                    "libgazebo_ros_init.so",
+                    "-s",
+                    "libgazebo_ros_factory.so",
+                    world_path,
+                ],
+                output="screen",
+            ),
+            joint_state_publisher_node,
+            robot_state_publisher_node,
+            spawn_entity,
+            robot_localization_node,
+            rviz_node,
+            # slam_async_launch,
+            # nav2_launch
+        ]
+    )
