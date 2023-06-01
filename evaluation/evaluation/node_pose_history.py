c5d8fd1f3341878339fd02111c438d1a65972eb4
b1n-ch1kn <albradford2468@gmail.com>
2023-06-01 18:03:14 +1000

    overhaul eval to also publish ground truth poses

--- evaluation/evaluation/node_pose_history.py
+++ evaluation/evaluation/node_pose_history.py
@@ -8,27 +8,41 @@ from geometry_msgs.msg import PoseWithCovarianceStamped, PoseStamped
 from nav_msgs.msg import Path
 
 class PoseHistory(Node):
-    path_history = Path()
-    last_path_time = time.time()
+    slam_path_history = Path()
+    last_slam_path_time = time.time()
+    gt_path_history = Path()
+    last_gt_path_time = time.time()
 
     def __init__(self) -> None:
         super().__init__("pose_history_node")
 
         # subscribe to topic
-        self.create_subscription(PoseWithCovarianceStamped, "/pose", self.callback, 10)
-        self.history_pub: Publisher = self.create_publisher(Path, "/slam/car_pose_history", 10)
+        self.create_subscription(PoseWithCovarianceStamped, "/slam/car_pose", self.slam_callback, 10)
+        self.create_subscription(PoseWithCovarianceStamped, "/ground_truth/car_pose", self.gt_callback, 10)
+        self.slam_history_pub: Publisher = self.create_publisher(Path, "/slam/car_pose_history", 10)
+        self.gt_history_pub: Publisher = self.create_publisher(Path, "/ground_truth/car_pose_history", 10)
 
         self.get_logger().info("---Track Writer Initalised---")
 
-    def callback(self, msg: PoseWithCovarianceStamped):
-        self.path_history.header = msg.header
+    def slam_callback(self, msg: PoseWithCovarianceStamped):
+        self.slam_path_history.header = msg.header
         pose = PoseStamped()
         pose.pose = msg.pose.pose
-        self.path_history.poses.append(pose)
+        self.slam_path_history.poses.append(pose)
 
-        if time.time() - self.last_path_time > 1:
-            self.history_pub.publish(self.path_history)
-            self.last_path_time = time.time()
+        if time.time() - self.last_slam_path_time > 1:
+            self.slam_history_pub.publish(self.slam_path_history)
+            self.last_slam_path_time = time.time()
+
+    def gt_callback(self, msg: PoseWithCovarianceStamped):
+        self.gt_path_history.header = msg.header
+        pose = PoseStamped()
+        pose.pose = msg.pose.pose
+        self.gt_path_history.poses.append(pose)
+
+        if time.time() - self.last_gt_path_time > 1:
+            self.gt_history_pub.publish(self.gt_path_history)
+            self.last_gt_path_time = time.time()
 
 
 def main():
