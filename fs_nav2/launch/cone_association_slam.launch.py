b7b623d4802a38d87b9e8c33fe1cdeb04ef9e7cf
b1n-ch1kn <albradford2468@gmail.com>
2023-06-01 17:29:10 +1000

    broke out stage 2 into a launch file

---
+++ fs_nav2/launch/cone_association_slam.launch.py
@@ -0,0 +1,58 @@
+import os
+
+import launch
+import launch_ros
+from launch.substitutions import Command, LaunchConfiguration
+
+
+def generate_launch_description():
+    pkg_share = launch_ros.substitutions.FindPackageShare(package="fs_nav2").find(
+        "fs_nav2"
+    )
+    slam_pkg_share = launch_ros.substitutions.FindPackageShare(
+        package="slam_toolbox"
+    ).find("slam_toolbox")
+
+    localisation_node = launch_ros.actions.Node(
+        package="robot_localization",
+        executable="ekf_node",
+        name="odom_filter_node",
+        output="screen",
+        parameters=[
+            os.path.join(pkg_share, "config/localisation_params.yaml"),
+            {"use_sim_time": LaunchConfiguration("use_sim_time")},
+        ],
+    )
+    async_slam_toolbox_node = launch_ros.actions.Node(
+        package='slam_toolbox',
+        executable='async_slam_toolbox_node',
+        name='slam_toolbox',
+        output='screen',
+        parameters=[
+          os.path.join(pkg_share, "config/slam_params.yaml"),
+          {"use_sim_time": LaunchConfiguration("use_sim_time")},
+        ],
+        remappings=[
+            ("/pose", "/slam/car_pose"),
+        ]
+
+    )
+    assocation_node = launch_ros.actions.Node(
+        package="cone_association",
+        executable="mapping2",
+        name="cone_association_node",
+        output="screen",
+    )
+
+    return launch.LaunchDescription(
+        [
+            launch.actions.DeclareLaunchArgument(
+                name="use_sim_time",
+                default_value="True",
+                description="Flag to enable use_sim_time",
+            ),
+            localisation_node,
+            async_slam_toolbox_node,
+            assocation_node,
+        ]
+    )
