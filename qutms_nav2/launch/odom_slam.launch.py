91349f269e5f38d7adb16d37c58270ea3304c761
Alastair <albradford2468@gmail.com>
2023-07-18 21:56:31 +1000

    reformat ekf slam with ekf odom - add navsat to odom

--- qutms_nav2/launch/odom_slam.launch.py
+++ qutms_nav2/launch/odom_slam.launch.py
@@ -1,26 +1,40 @@
 import os
 
 import launch
-import launch_ros
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 from launch.substitutions import Command, LaunchConfiguration
 
 
 def generate_launch_description():
-    pkg_share = launch_ros.substitutions.FindPackageShare(package="qutms_nav2").find(
+    pkg_share = FindPackageShare(package="qutms_nav2").find(
         "qutms_nav2"
     )
 
-    odom_localization_node = launch_ros.actions.Node(
+    # Community ROS 2 packages
+    localisation_node = Node(
         package="robot_localization",
         executable="ekf_node",
         name="odom_filter_node",
         output="screen",
         parameters=[
-            os.path.join(pkg_share, "config/localisation_params.yaml"),
+            os.path.join(pkg_share, "config/dual_localisation_params.yaml"),
             {"use_sim_time": LaunchConfiguration("use_sim_time")},
         ],
     )
-    slam_node = launch_ros.actions.Node(
+    navsat_transform_node = Node(
+        package="robot_localization",
+        executable="navsat_transform_node",
+        name="navsat_transform_node",
+        output="screen",
+        parameters=[
+            os.path.join(pkg_share, "config/dual_localisation_params.yaml"),
+            {"use_sim_time": LaunchConfiguration("use_sim_time")},
+        ],
+        remappings=[('gps/fix', 'imu/nav_sat_fix'), ('imu', 'imu/data')]
+    )
+
+    slam_node = Node(
         package="py_slam",
         executable="odom_slam",
         name="odom_slam_node",
@@ -34,7 +48,8 @@ def generate_launch_description():
                 default_value="True",
                 description="Flag to enable use_sim_time",
             ),
-            odom_localization_node,
+            localisation_node,
+            navsat_transform_node,
             slam_node,
         ]
     )
